5aad750edca5e5fb5c7f29fac6c49ac53550519b
Olavo <olavo.santos@skipthedishes.ca>
2020-04-13 17:51:39 -0500

    IMPROVE: (CORE) updated Router definition

--- packages/core/src/Router.ts
+++ packages/core/src/Router.ts
@@ -4,4 +4,5 @@ export interface Router {
   has(path: string): boolean;
   routes(namespace?: string): Route[];
   find(path: string): Route | undefined;
+  find(namespace: string, path: string): Route | undefined;
 }
