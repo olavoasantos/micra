edf05e2002538cd83e00113b1618fd5c6c156c63
Olavo <olavo.santos@skipthedishes.ca>
2020-04-14 16:06:09 -0500

    FEAT: (VALIDATION-RULES) added isString rule

---
+++ packages/validation-rules/src/isString/tests/isString.test.ts
@@ -0,0 +1,21 @@
+import { isString } from '../index';
+import { mockContext } from '../../helpers/mockContext';
+
+describe('isString rule', () => {
+  /** @test */
+  it('should return true if the value is a string', () => {
+    expect(
+      isString().check(mockContext({ value: 'some string' })),
+    ).toBeTruthy();
+  });
+
+  /** @test */
+  it('should return false if the value is not a string', () => {
+    expect(isString().check(mockContext({ value: 123 }))).toBeFalsy();
+    expect(isString().check(mockContext({ value: {} }))).toBeFalsy();
+    expect(isString().check(mockContext({ value: true }))).toBeFalsy();
+    expect(isString().check(mockContext({ value: null }))).toBeFalsy();
+    expect(isString().check(mockContext({ value: undefined }))).toBeFalsy();
+    expect(isString().check(mockContext({ value: /\s/g }))).toBeFalsy();
+  });
+});
