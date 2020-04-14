2865c4d11e013b0a6c202d9493ef3325660f7c59
Olavo <olavo.santos@skipthedishes.ca>
2020-04-14 15:44:49 -0500

    FEAT: (VALIDATION-RULES) created library and added the email rule

---
+++ packages/validation-rules/src/email/index.ts
@@ -0,0 +1,9 @@
+import { ValidationContext } from '@micra/validator';
+
+export const email = () => ({
+  check({ value }: ValidationContext) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(value).toLowerCase());
+  },
+  message: () => `validation.email`,
+});
