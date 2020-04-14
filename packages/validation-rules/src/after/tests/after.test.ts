bfb52ff55f49ba7b6148091711f81a4d2cbf2748
Olavo <olavo.santos@skipthedishes.ca>
2020-04-14 18:13:36 -0500

    FEAT: (VALIDATION-RULES) added after rule

---
+++ packages/validation-rules/src/after/tests/after.test.ts
@@ -0,0 +1,25 @@
+import { after } from '../index';
+import { mockContext } from '../../helpers/mockContext';
+
+describe('after rule', () => {
+  /** @test */
+  it('should return true when a date is after another date', () => {
+    expect(
+      after('2013-04-28').check(mockContext({ value: '2013-04-29' })),
+    ).toBeTruthy();
+  });
+
+  /** @test */
+  it('should return false when a date is equal to another date', () => {
+    expect(
+      after('2013-04-29').check(mockContext({ value: '2013-04-29' })),
+    ).toBeFalsy();
+  });
+
+  /** @test */
+  it('should return false when a date is before another date', () => {
+    expect(
+      after('2013-04-30').check(mockContext({ value: '2013-04-29' })),
+    ).toBeFalsy();
+  });
+});
