7293f535dfe7be0c22d819f3b892e299f9d3ed72
Olavo <olavo.santos@skipthedishes.ca>
2020-04-14 15:46:10 -0500

    FIX: (VALIDATION-RULES) linted project

--- packages/validation-rules/src/helpers/mockContext.ts
+++ packages/validation-rules/src/helpers/mockContext.ts
@@ -1,13 +1,14 @@
 import { ValidationContext, ValidationError } from '@micra/validator';
 
-export const mockContext = (overrides: Partial<ValidationContext>) => ({
-  data: {
-    fieldName: null
-  },
-  field: 'fieldName',
-  value: null,
-  dto: {},
-  fields: ['fieldName'],
-  errors: new ValidationError(overrides.fields || ['fieldName']),
-  ...overrides,
-}) as ValidationContext;
+export const mockContext = (overrides: Partial<ValidationContext>) =>
+  ({
+    data: {
+      fieldName: null,
+    },
+    field: 'fieldName',
+    value: null,
+    dto: {},
+    fields: ['fieldName'],
+    errors: new ValidationError(overrides.fields || ['fieldName']),
+    ...overrides,
+  } as ValidationContext);
