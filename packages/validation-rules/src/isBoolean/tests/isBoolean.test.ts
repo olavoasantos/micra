6c870dc1a8f8d166f018864cf54040d4d11a07df
Olavo <olavo.santos@skipthedishes.ca>
2020-04-14 19:29:30 -0500

    FEAT: (VALIDATION-RULES) added isBoolean rule

---
+++ packages/validation-rules/src/isBoolean/tests/isBoolean.test.ts
@@ -0,0 +1,21 @@
+import { isBoolean } from '../index';
+import { mockContext } from '../../helpers/mockContext';
+
+describe('isBoolean rule', () => {
+  /** @test */
+  it('should return true if the value is an boolean', () => {
+    expect(isBoolean().check(mockContext({ value: true }))).toBeTruthy();
+    expect(isBoolean().check(mockContext({ value: false }))).toBeTruthy();
+  });
+
+  /** @test */
+  it('should return false if the value is not an boolean', () => {
+    expect(isBoolean().check(mockContext({ value: 'string' }))).toBeFalsy();
+    expect(isBoolean().check(mockContext({ value: 123 }))).toBeFalsy();
+    expect(isBoolean().check(mockContext({ value: [] }))).toBeFalsy();
+    expect(isBoolean().check(mockContext({ value: {} }))).toBeFalsy();
+    expect(isBoolean().check(mockContext({ value: null }))).toBeFalsy();
+    expect(isBoolean().check(mockContext({ value: undefined }))).toBeFalsy();
+    expect(isBoolean().check(mockContext({ value: /\s/g }))).toBeFalsy();
+  });
+});
