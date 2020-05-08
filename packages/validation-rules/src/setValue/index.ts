e77e18b4fd4dceb7bdad088127bc51d9aede8a8b
Olavo <olavo.santos@skipthedishes.ca>
2020-05-08 11:23:00 -0500

    LINT: (VALIDATION-RULES) linted project

--- packages/validation-rules/src/setValue/index.ts
+++ packages/validation-rules/src/setValue/index.ts
@@ -1,9 +1,6 @@
 import { ValidationContext } from '@micra/validator';
 
-export const setValue = (value?: any) => ({
-  dto,
-  field,
-}: ValidationContext) => {
+export const setValue = (value?: any) => ({ dto, field }: ValidationContext) => {
   dto[field as string] = value;
 
   return [];
