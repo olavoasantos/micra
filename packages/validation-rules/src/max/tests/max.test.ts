678a0661036980132ce4dea3a4434b98af56a2d6
Olavo <olavo.santos@skipthedishes.ca>
2020-05-23 18:43:29 -0500

    FIX: (VALIDATION-RULES) fixed max rule tests

--- packages/validation-rules/src/max/tests/max.test.ts
+++ packages/validation-rules/src/max/tests/max.test.ts
@@ -2,15 +2,15 @@ import { max } from '../index';
 import { mockContext } from '../../helpers/mockContext';
 
 describe('max rule', () => {
-  it('should return true when a number greater than another number', () => {
-    expect(max({ value: 100 }).check(mockContext({ value: 200 }))).toBeTruthy();
+  it('should return true when a number smaller than another number', () => {
+    expect(max({ value: 100 }).check(mockContext({ value: 50 }))).toBeTruthy();
   });
 
   it('should return false when a number is equal to another number', () => {
     expect(max({ value: 100 }).check(mockContext({ value: 100 }))).toBeFalsy();
   });
 
-  it('should return false when a number is not greater than another number', () => {
-    expect(max({ value: 100 }).check(mockContext({ value: 50 }))).toBeFalsy();
+  it('should return false when a number is not smaller than another number', () => {
+    expect(max({ value: 100 }).check(mockContext({ value: 200 }))).toBeFalsy();
   });
 });
