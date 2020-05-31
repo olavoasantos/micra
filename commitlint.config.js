42a4372a36323a1ad5112081738d44d36aa8e342
Olavo <olavo.santos@skipthedishes.ca>
2020-05-31 11:40:34 -0500

    FEAT: (BASE) tweaking on apollo-lambda-kernel and created test-utils package

--- commitlint.config.js
+++ commitlint.config.js
@@ -37,6 +37,7 @@ module.exports = {
       'REACT-STORE-HOOKS',
       'SERVICE-PROVIDER',
       'STORAGE-WRAPPER',
+      'TEST-UTILS',
       'TSYRINGE-SERVICE-CONTAINER',
       'VALIDATION-RULES',
       'VALIDATOR',
