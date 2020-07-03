988072e8715080b324f960e87956b7072590c691
Olavo <olavo.santos@skipthedishes.ca>
2020-07-03 10:19:36 -0500

    DEPENDENCIES: (BASE) updated dependencies

--- packages/core/src/Config.ts
+++ packages/core/src/Config.ts
@@ -2,7 +2,7 @@ import { Static } from './types';
 
 export interface Config {
   has(key: string): boolean;
-  set(key: string, value: any): void;
+  set<T = any>(key: string, value: T): void;
   get<T = any>(key: string): T | undefined;
 }
 
