e7d6c39343a769bf1803bef1709235df62d1fc22
Olavo <olavo.santos@skipthedishes.ca>
2020-07-10 18:13:41 -0500

    FEAT: (PATH-MATCH) added helpers and organized definitions

---
+++ packages/path-match/src/helpers.ts
@@ -0,0 +1,58 @@
+import { PathObject } from './types';
+
+export const escapeIfExists = (value: string): string => (value ? `\\${value}` : '');
+
+const parseQueryParams = (params: Record<string, any>) =>
+  Object.entries(params).reduce(
+    (qs, [key, value]) => `${qs}${qs === '' ? '?' : '&'}${key}=${value}`,
+    '',
+  );
+
+export const toTest = (definition: PathObject) => (path: string) =>
+  definition.regex.test(path) &&
+  !(
+    definition.couldHaveDoubleSeparator &&
+    path.includes(definition.options.PATH_SEPARATOR + definition.options.PATH_SEPARATOR)
+  );
+
+export const toMatch = (definition: PathObject) => <T = Record<string, any>>(path: string) => {
+  if (definition.variables.length === 0) {
+    return {};
+  }
+
+  const result = definition.regex.exec(path);
+  if (!result) {
+    return {};
+  }
+
+  return definition.variables.reduce((variables: any, variable, index) => {
+    variables[variable.name] = result[index + 1];
+    return variables;
+  }, {}) as T;
+};
+
+export const toPathBuilder = (definition: PathObject) => (
+  params: Record<string, any> = {},
+  query: Record<string, any> = {},
+) => {
+  const qs = parseQueryParams(query);
+  const definedParams = Object.keys(params);
+  const path = definition.elements.reduce((partial: string, element) => {
+    if (element.type === 'STATIC') {
+      return `${partial}${definition.options.PATH_SEPARATOR}${element.value}`;
+    }
+
+    if (element.isOptional && !definedParams.includes(element.name)) {
+      return partial;
+    }
+
+    const value = String(params[element.name]);
+    if (!element.hasOptions || element.options.includes(value)) {
+      return `${partial}${definition.options.PATH_SEPARATOR}${value}`;
+    }
+
+    return partial;
+  }, '');
+
+  return path + qs;
+};
--- packages/path-match/src/tests/path.test.ts
+++ packages/path-match/src/tests/path.test.ts
@@ -1,7 +1,17 @@
 import { path } from '../path';
+import { toMatch, toPathBuilder, toTest } from '../helpers';
 
 it('should pass', () => {
   const parsePath = path();
 
-  parsePath(':lng(fr)?/restaurants/:city?');
+  const route = parsePath(':lng(fr)?/restaurants/:city?');
+  const test = toTest(route);
+  const match = toMatch(route);
+  const pathTo = toPathBuilder(route);
+
+  console.log({
+    test: test('fr/restaurants//'),
+    match: match('restaurants/winnipeg'),
+    pathTo: pathTo({ lng: 'fr', city: 'toronto' }),
+  });
 });
