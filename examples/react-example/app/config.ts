f15c7f497d61be45f91cb1f2dcc61d62208d72d8
Olavo <olavo.santos@skipthedishes.ca>
2020-07-20 12:19:45 -0500

    FIX: (REACT-EXAMPLE) cleaned env

--- examples/react-example/app/config.ts
+++ examples/react-example/app/config.ts
@@ -1,11 +1,12 @@
 import app from '@micra/application';
+import { ReactDomKernel } from '@micra/react-dom-kernel';
 import { TSyringeServiceContainer } from '@micra/tsyringe-service-container';
 import { AppConfig } from 'app/types';
 
 app.config.set<AppConfig>('app', {
   container: TSyringeServiceContainer,
 
-  kernel: undefined,
+  kernel: ReactDomKernel,
 
   services: [
     //
