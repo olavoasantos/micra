5a3c920b33f56ba3108fc1983eedd84402aa303d
Olavo <olavo.santos@skipthedishes.ca>
2020-07-21 17:24:52 -0500

    FEAT: (REACT-ROUTE-REGISTRY) added defaults to all and find methods

--- packages/react-route-registry/src/ReactRouteRegistry.ts
+++ packages/react-route-registry/src/ReactRouteRegistry.ts
@@ -2,7 +2,18 @@ import React from 'react';
 import loadable from '@loadable/component';
 import { BaseRoute, BaseRouteRegistryBuilder, Route } from '@micra/route-registry';
 
-export class ReactRouteRegistry extends BaseRouteRegistryBuilder<'page', React.ComponentType<any>> {
+export class ReactRouteRegistry<T extends string = string> extends BaseRouteRegistryBuilder<
+  'page' | T,
+  React.ComponentType<any>
+> {
+  all(verb: 'page' | T = 'page') {
+    return super.all(verb);
+  }
+
+  find(path: string, verb: 'page' | T = 'page') {
+    return super.find(path, verb);
+  }
+
   page(
     path: string,
     handler:
@@ -11,13 +22,17 @@ export class ReactRouteRegistry extends BaseRouteRegistryBuilder<'page', React.C
   ) {
     let route: BaseRoute<React.ComponentType<any>>;
     if (typeof handler === 'function') {
-      route = new BaseRoute<React.ComponentType<any>>(this.routes, path, loadable<React.ComponentType<any>>(async () => {
-        for (const middleware of route.middleware) {
-          await middleware(route);
-        }
+      route = new BaseRoute<React.ComponentType<any>>(
+        this.routes,
+        path,
+        loadable<React.ComponentType<any>>(async () => {
+          for (const middleware of route.middleware) {
+            await middleware(route);
+          }
 
-        return await handler();
-      }));
+          return await handler();
+        }),
+      );
 
       route.options = { exact: true };
     } else {
--- packages/react-route-registry/src/index.ts
+++ packages/react-route-registry/src/index.ts
@@ -4,4 +4,6 @@ import { ReactRouteRegistry } from './ReactRouteRegistry';
 
 export * from './ReactRouteRegistry';
 export type ReactRoute = Route<React.ComponentType<any>>;
-export const router = new ReactRouteRegistry(new BaseRouteRegistry<'page', React.ComponentType<any>>());
+export const router = new ReactRouteRegistry(
+  new BaseRouteRegistry<'page', React.ComponentType<any>>(),
+);
