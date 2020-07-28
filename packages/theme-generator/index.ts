50a1208f6f85cc63865b8c39b6565b809b6f646d
Olavo <olavo.santos@skipthedishes.ca>
2020-07-27 19:04:37 -0500

    FIX: (THEME-GENERATOR) fixed exports

--- packages/theme-generator/index.ts
+++ packages/theme-generator/index.ts
@@ -4,4 +4,6 @@ export * from './themeGenerator';
 export * from './generators/toCssVariables';
 export * from './generators/toCssVariablesThemeObject';
 export * from './generators/toThemeType';
+export * from './generators/toGenericThemeType';
+export * from './generators/toThemeObject';
 export * from './generators/types';
